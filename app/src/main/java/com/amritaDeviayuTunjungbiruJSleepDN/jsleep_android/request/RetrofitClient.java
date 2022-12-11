dfa26b97347f4ecb1345b815877005ccb4ac2291
birujung <amrita.deviayu@ui.ac.id>
2022-12-11 17:11:57 +0700

    JAVADOC Request Done

--- app/src/main/java/com/amritaDeviayuTunjungbiruJSleepDN/jsleep_android/request/RetrofitClient.java
+++ app/src/main/java/com/amritaDeviayuTunjungbiruJSleepDN/jsleep_android/request/RetrofitClient.java
@@ -3,9 +3,24 @@ package com.amritaDeviayuTunjungbiruJSleepDN.jsleep_android.request;
 import retrofit2.*;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+/**
+ * The {@code RetrofitClient} class provides a Retrofit client for making HTTP requests.
+ *
+ * @author Amrita Deviayu Tunjungbiru
+ * @version 1.0
+ */
 public class RetrofitClient {
+    /**
+     * The Retrofit client instance.
+     */
     private static Retrofit retrofit = null;
 
+    /**
+     * Returns a Retrofit client with the specified base URL.
+     *
+     * @param baseUrl the base URL for the client
+     * @return a Retrofit client with the specified base URL
+     */
     public static Retrofit getClient(String baseUrl) {
         if(retrofit == null) {
             retrofit = new Retrofit.Builder()
